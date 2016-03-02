7e6712eecc8f1448c5dc76ee7238979d720be91c
liuquan <liuquan@smartdevices.com.cn>
2016-03-02 20:48:34 +0800

    Init: init eureka service

---
+++ eureka-service/src/main/java/com/umasuo/service/eureka/Application.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2015-2016 Umasuo, Inc. All Rights Reserved.
+ */
+
+package com.umasuo.service.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
