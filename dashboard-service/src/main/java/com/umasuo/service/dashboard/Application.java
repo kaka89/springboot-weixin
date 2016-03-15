043bb931b83ff23f2ea0ea95802e427ebbfdaf02
liuquan <liuquan@smartdevices.com.cn>
2016-03-15 15:27:51 +0800

    DashBoard: init

--- dashboard-service/src/main/java/com/umasuo/service/dashboard/Application.java
+++ dashboard-service/src/main/java/com/umasuo/service/dashboard/Application.java
@@ -2,19 +2,24 @@
  * Copyright (c) 2015-2016 Umasuo, Inc. All Rights Reserved.
  */
 
-package com.umasuo.service.config;
+package com.umasuo.service.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
-@EnableConfigServer
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableDiscoveryClient
+@EnableHystrixDashboard
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        
+
     }
 
 }
